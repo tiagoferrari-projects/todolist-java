3fb21402592f2b25862ad2b4f0096993d53da195
Tiago Ferrari <tiago-projects@outlook.com>
2023-10-11 02:30:09 -0300

    Aula 02
    Integração com Banco de Dados

--- src/main/java/com/tiagoprojects/todolist/user/UserController.java
+++ src/main/java/com/tiagoprojects/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package com.tiagoprojects.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,21 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/users")
 public class UserController {
 
-    /**
-     * String (texto)
-     * Integer (int) números inteiros
-     * Double (double) Números 0.0000
-     * Float (float) Números 0.000
-     * char (A C)
-     * Date (data)
-     * void (não tem retorno)
-     */
+    @Autowired
+    private IUserRepository userRepository;
 
-    /*
-     * Body
-     */
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel) {
-        System.out.println(userModel.getUsername());
+    public ResponseEntity create(@RequestBody UserModel userModel) {
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
